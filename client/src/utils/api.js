994264b8fff4b65917c25d46b22f66e77c4f344c
sarathchandra2004 <ysarath2022@gmail.com>
2025-08-03 13:10:42 +0530

    Deployed Backend and added it to frontend

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
@@ -16,9 +16,7 @@ api.interceptors.request.use(
     }
     return config
   },
-  (error) => {
-    return Promise.reject(error)
-  }
+  (error) => Promise.reject(error)
 )
 
 // Response interceptor to handle auth errors
@@ -31,4 +29,4 @@ api.interceptors.response.use(
     }
     return Promise.reject(error)
   }
-) 
+)
